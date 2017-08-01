21bda43a30dcda0ae75710aeec882ea593a52fac
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-08-01 14:50:47 +0100

    release_bike method added

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,8 @@
 require "docking_station"
 
 describe DockingStation do
-	
+	it "should respond to release_bike method" do
+		DockingStation.new.release_bike == true
+		# expect(subject).to respond_to :release_bike
+	end 
 end
